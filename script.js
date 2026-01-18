09cdcd30652d9a64cabb3b7cdeca36e1088c91f0
Genevieve_P <genevievepala2010@gmail.com>
2026-01-17 22:38:48 -0500

    Adding some popping up to the navigation tabs and added another html page.

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ if(toggleButton) {
 } else {
     console.warn('bgToggle button not found');
 }
+
+const links = document.querySelectorAll(".nav-links a");
+const currentPage = window.location.pathname.split("/").pop() || "index1.html";
+
+console.log("Current page:", currentPage);
+
+links.forEach(link => {
+    const href = link.getAttribute("href");
+    console.log("Checking link:", href);
+    if(href === currentPage) {
+        console.log("Match found, adding active class to:", href);
+        link.classList.add("active");
+    }
+});
